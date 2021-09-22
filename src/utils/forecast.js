d31bae4380196356783894399c7f715c3859174d
Thomas <thomasjosephm@gmail.com>
2021-09-22 16:26:56 +0530

    Init commit

---
+++ src/utils/forecast.js
@@ -0,0 +1,17 @@
+const request = require('request')
+
+const forecast = (latitude, longitude, callback) => {
+    const url = 'http://api.weatherstack.com/current?access_key=dadde64f5436741619d8777f15e9f9b5&query=' + latitude + ',' + longitude + '&unit=f'
+
+    request({ url, json: true }, (error, { body }) => {
+        if (error) {
+            callback('Unable to connect to weather service!', undefined)
+        } else if (body.error) {
+            callback('Unable to fetch weather', undefined)
+        } else {
+            callback(undefined, body.current.weather_descriptions[0] + ' It is currently ' + body.current.temperature + ' degress out, though it feels like ' + body.current.feelslike + ' degrees!')
+        }
+    })
+}
+
+module.exports = forecast
